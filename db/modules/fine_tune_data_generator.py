fd5d93050859ee691f920179e3afd5d2a0bee82b
navinpandiyan <navinpandiyan@gmail.com>
2025-10-19 17:12:43 +0530

    reduce sample size

--- db/modules/fine_tune_data_generator.py
+++ db/modules/fine_tune_data_generator.py
@@ -47,7 +47,7 @@ TERM_QUERIES = [
 # Fine-Tuning Generator
 # -----------------------------
 # Conversational fine-tuning generator
-def generate_insurance_fine_tuning_data_dialogue(hospitals, hospital_insurance_links, num_samples_per_hospital=15):
+def generate_insurance_fine_tuning_data_dialogue(hospitals, hospital_insurance_links, num_samples_per_hospital=3):
     fine_tuning_examples = []
 
     # Map hospital_id -> list of insurance plans
